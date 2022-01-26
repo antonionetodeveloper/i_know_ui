0bc875c77f9871a425d39c45994959520b262c54
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-01-26 19:04:14 -0300

    alter prop rel from </a>

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,7 +3,7 @@ import { Container } from "./styles";
 export const Footer = () => {
   return (
     <Container>
-      <a href="https://storyset.com/people" target="_blank" rel="" >People illustrations by Storyset</a>
+      <a href="https://storyset.com/people" target="_blank" rel="noreferrer" >People illustrations by Storyset</a>
     </Container>
   );
 }
